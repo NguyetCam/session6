afeffdf2518e7e1f2ff26462ce5ceee99b7f5657
NguyetCam <96852028+NguyetCam@users.noreply.github.com>
2022-03-31 23:48:23 +0700

    Update StudentList.java

--- session6lab1/StudentList.java
+++ session6lab1/StudentList.java
@@ -126,13 +126,13 @@ public class StudentList {
             Scanner sc = new Scanner(System.in);
             String id = sc.nextLine();
             for(Student st:StudentList){
-                if(id.compareTo(st.getId())!=0){
-                    System.out.println("This ID is not available");
+                if(id.equals(st.getId())){
+                    System.out.println("Student " + st.getName() + " has been removed");
+                    StudentList.remove(st);
                     return;
                 }
             }
-            StudentList.removeIf(st -> id.equals(st.getId()));
-            System.out.println("Student ID " + id + " has been removed");
+            System.out.println("This ID is not available");
         }
     }
 
@@ -145,22 +145,22 @@ public class StudentList {
         if(StudentList.size()==0) System.out.println("There aren't any students in the list");
         else {
             for(int i=0;i<StudentList.size();i++){
-                for(int j=0;j<StudentList.size();j++){
-                    if(StudentList.get(i).getGpa()>StudentList.get(j).getGpa()){
-                        gpatemp = StudentList.get(j).getGpa();
-                        StudentList.get(j).setGpa(StudentList.get(i).getGpa());
+                for (Student student : StudentList) {
+                    if (StudentList.get(i).getGpa() > student.getGpa()) {
+                        gpatemp = student.getGpa();
+                        student.setGpa(StudentList.get(i).getGpa());
                         StudentList.get(i).setGpa(gpatemp);
-                        idtemp = StudentList.get(j).getId();
-                        StudentList.get(j).setId(StudentList.get(i).getId());
+                        idtemp = student.getId();
+                        student.setId(StudentList.get(i).getId());
                         StudentList.get(i).setId(idtemp);
-                        nametemp = StudentList.get(j).getName();
-                        StudentList.get(j).setName(StudentList.get(i).getName());
+                        nametemp = student.getName();
+                        student.setName(StudentList.get(i).getName());
                         StudentList.get(i).setName(nametemp);
-                        addresstemp = StudentList.get(j).getAddress();
-                        StudentList.get(j).setAddress(StudentList.get(i).getAddress());
+                        addresstemp = student.getAddress();
+                        student.setAddress(StudentList.get(i).getAddress());
                         StudentList.get(i).setAddress(addresstemp);
-                        bdaytemp = StudentList.get(j).getBday();
-                        StudentList.get(j).setBday(StudentList.get(i).getBday());
+                        bdaytemp = student.getBday();
+                        student.setBday(StudentList.get(i).getBday());
                         StudentList.get(i).setBday(bdaytemp);
                     }
                 }
